ccc5b384bad9e985ad6e0465d9700cc39959e839
Krabbens <s188660@student.pg.edu.pl>
2024-07-15 05:42:04 +0200

    chore: Add MQTT test functionality for subscribing to "esp32/ledarray"

---
+++ mqtt_test/main.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc, props):
+    print("Connected with result code " + str(rc))
+    client.subscribe("esp32/ledarray")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.tls_set()
+client.username_pw_set("esp", "_ledarray_nugget")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("5686adbdc3644dca8e63a851e72c3b21.s1.eu.hivemq.cloud", 8883, 60)
+
+client.publish("esp32/ledarray", "Hello from Python!")
+
+client.loop_forever()
