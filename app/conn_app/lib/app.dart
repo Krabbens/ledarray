15f4a31b33c2ef5cdc42baf2dfa102c45cb4388c
Krabbens <s188660@student.pg.edu.pl>
2024-07-18 04:33:05 +0200

    feat: Add PreConnectionRouter for pre-connection setup

--- app/conn_app/lib/app.dart
+++ app/conn_app/lib/app.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes: {
+        '/pre-connection': (context) => const PreConnectionRouter(),
         '/scan-wifi': (context) => const ScanWifi(),
         '/permission': (context) => const PermissionView(),
       },
