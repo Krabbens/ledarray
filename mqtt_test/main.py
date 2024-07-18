f046908288176361a329099755fdd35bac4cc13c
Krabbens <s188660@student.pg.edu.pl>
2024-07-18 02:15:33 +0200

    chore: Update MQTT subscribe topic to "esp32/check_alive" and publish "esp32/alive_status"

--- mqtt_test/main.py
+++ mqtt_test/main.py
@@ -2,9 +2,10 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc, props):
     print("Connected with result code " + str(rc))
-    client.subscribe("esp32/ledarray")
+    client.subscribe("esp32/check_alive")
 
 def on_message(client, userdata, msg):
+    client.publish("esp32/alive_status", 1)
     print(msg.topic + " " + str(msg.payload))
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -15,7 +16,5 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("5686adbdc3644dca8e63a851e72c3b21.s1.eu.hivemq.cloud", 8883, 60)
-
-client.publish("esp32/alive_status", 1)
 
 client.loop_forever()
