759e23115bb61b751a7a845044f711dd6b0329d7
Kosma <s188660@pg.edu.pl>
2024-08-30 01:36:01 +0200

    feat: Improve image compression script

--- preprocess/compress.py
+++ preprocess/compress.py
@@ -13,13 +13,28 @@ previous_image = None
 def convert_to_palette(pixel):
     return pixel[0]
 
+change = False
+
 for file in files:
     current_image = Image.open(os.path.join(path, file))
     
     if previous_image is not None:
         for index, image in enumerate(zip(previous_image.getdata(), current_image.getdata())):
             if convert_to_palette(image[0]) != convert_to_palette(image[1]):
-                diff += struct.pack('hB', index, convert_to_palette(image[1]))
+                if index > 200 and change == False:
+                    diff += struct.pack('B', 201)
+                    diff += struct.pack('BB', index - 200, convert_to_palette(image[1]))
+                    change = True
+                elif index <= 200 and change == True:
+                    diff += struct.pack('B', 202)
+                    diff += struct.pack('BB', index, convert_to_palette(image[1]))
+                    change = False
+                else:
+                    if index > 200:
+                        diff += struct.pack('BB', index - 200, convert_to_palette(image[1]))
+                    else:
+                        diff += struct.pack('BB', index, convert_to_palette(image[1]))
+                
 
         print(f'Compared {file} with {previous_image.filename}')
     
